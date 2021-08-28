13304afa1ae36c492ced14bb800b1988e701259c
Qiantan Hong <qhong@mit.edu>
2021-08-28 04:52:34 -0700

    Update copyright, assign version number

--- crdt.el
+++ crdt.el
@@ -1,24 +1,27 @@
 ;;; crdt.el --- collaborative editing using Conflict-free Replicated Data Types  -*- lexical-binding: t; -*-
-;;
-;; Copyright (C) 2020 Qiantan Hong
-;;
+
+;; Copyright (C) 2021 Free Software Foundation, Inc.
+
 ;; Author: Qiantan Hong <qhong@alum.mit.edu>
 ;; Maintainer: Qiantan Hong <qhong@alum.mit.edu>
+;; URL: https://code.librehq.com/qhong/crdt.el
 ;; Keywords: collaboration crdt
-;; Version: 0.0.0
-;;
-;; crdt.el is free software: you can redistribute it and/or modify
+;; Version: 0.1.0
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
-;; crdt.el is distributed in the hope that it will be useful,
+
+;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
-;; along with crdt.el.  If not, see <https://www.gnu.org/licenses/>.
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; This package provides a collaborative editing environment for Emacs.
